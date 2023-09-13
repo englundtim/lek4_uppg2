f82c4ef5ab728333e64e8d30bd890b2ed72e4753
Tim <tim.englund@elev.ga.ntig.se>
2023-09-13 10:31:00 +0200

    Lade till bokstavssökning

--- src/App.java
+++ src/App.java
@@ -8,6 +8,11 @@ public class App {
 
         int letters = word.length();
         System.out.println("\""+word+"\""+" har "+letters+" bokstäver i sig."); 
+
+        System.out.println("Välj en bokstav som programmet ska leta efter");
+        String seek = in.nextLine();
+        int seek_index = word.indexOf(seek);
+        System.out.println("Bokstaven ligger på plats "+(seek_index+1));
         
         
     }

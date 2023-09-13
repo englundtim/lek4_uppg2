72754dfe61a9f8563f0f38691ea4a7f920c5a4d0
Tim <tim.englund@elev.ga.ntig.se>
2023-09-13 10:14:58 +0200

    Lade till beräkning av antal ord i en text

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Mata in en text på ett valfritt antal ord");
+        String word = in.nextLine();
+
+        int letters = word.length();
+        System.out.println("\""+word+"\""+" har "+letters+" bokstäver i sig."); 
+        
+        
     }
 }
